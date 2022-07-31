fd88f541d1dc42e632e5ea1779245493062fdadf
sobahan-ali <sobahaan.ali@gmail.com>
2022-07-31 13:14:08 +0600

    javascript practice problem solution done

---
+++ calculate.js
@@ -0,0 +1,42 @@
+// practice problem -1
+
+var momMoney = 1000;
+
+var apple = 300;
+var orange = 400;
+
+var totalCost = apple + orange;
+
+var shopKeeperReturn = momMoney - totalCost;
+
+console.log(" the shopkeeper will return to harry =" , shopKeeperReturn + " taka")
+
+// practice problem -2
+var mathematics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50
+
+var totalMarks = mathematics + biology + chemistry + physics + bangla;
+
+var averageMarks = totalMarks/5;
+averageMarks = averageMarks.toFixed(2);
+
+console.log("average marks is ", averageMarks);
+
+// practice problem -3
+
+var first = 'I am going to be';
+var second = 'an awesome web developer';
+
+var combine = first + " " + second;
+console.log(combine);
+
+// practice problem -4
+
+var num = 119;
+
+var remainder = num % 5;
+
+console.log(remainder);
